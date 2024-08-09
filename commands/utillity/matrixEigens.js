1fb89534567f5bc7c957a37658bd9f3a427649de
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-08-08 20:53:50 -0400

    chore:fixed the issue with eigenvalues almost

--- commands/utillity/matrixEigens.js
+++ commands/utillity/matrixEigens.js
@@ -25,15 +25,51 @@ module.exports = {
         JSON.parse(interaction.options.get("matrix").value)
       );
       const result = math.eigs(matrix);
-      const eigenvalues = result.values;
+      // This will retrieve the length fo eigenvalues --> we can get the eigenvalues in an array formating using the following code:
+      // Object.values(result.values)[0]; instead
+      const eigenvalues = Object.values(result.values)[0];
+      // we can use this as a while loop to loop thoguh to determine the objects
+      const numberOfEigenvalues = Object.values(result.values)[1][0];
+      // TODO : this is a test, it should be printing out the vectors corresponding to the second eigenvalue in this case
       const eigenvectors = result.eigenvectors;
+      const singleEigenvector = eigenvectors[1];
 
-      //console.log(`The resulting onject is ${JSON.stringify(result)}`);
+      // define a function that will be used to print out the eigenvectors and save them in an array, which will then be called when this command is executed
+
+      const getEigenvectors = async () => {
+        let iterator = 0;
+        // we will be storing the eigenvectors here
+        let eigenvectors = [];
+        while (iterator < numberOfEigenvalues) {
+          const correspondingEigenvectors = await Object.values(
+            Object.values(
+              math.eigs(
+                math.matrix(JSON.parse(interaction.options.get("matrix").value))
+              ).eigenvectors[iterator]
+            )[iterator]
+          )[0];
+          eigenvectors.push(correspondingEigenvectors);
+          iterator++;
+        }
+        return eigenvectors;
+      };
+
+      // call on the function to get the eigenvectors
+      const eigenvectorsList = await getEigenvectors();
+      console.log(eigenvectorsList);
+      //console.log(`The resulting onject is ${JSON.stringify(result.eigenvectors)}`);
       console.log(
-        `The eigenvalues are ${JSON.stringify(
-          eigenvalues
-        )}, and the corresponding eigenvectors are ${JSON.stringify(
-          eigenvectors[1].vector
+        `The number of eigenvalues are ${JSON.stringify(
+          numberOfEigenvalues
+        )} and they are ${eigenvalues}, and the corresponding eigenvectors are ${JSON.stringify(
+          Object.values(
+            Object.values(
+              math.eigs(
+                math.matrix(JSON.parse(interaction.options.get("matrix").value))
+              ).eigenvectors[1]
+            )[1]
+            // we will need to ensure we are always acessing the values in the 0th index since this is where the eigenvalues will be located
+          )[0]
         )}`
       );
       await interaction.editReply({
