06f6a7dfea3da43af974d5b23cf80f8256037119
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-08-14 22:37:15 -0400

    feat:bootleg but working implementation of linear independence

--- commands/utillity/LinearIndependence.js
+++ commands/utillity/LinearIndependence.js
@@ -1,20 +1,51 @@
-/**
- * @Purpose This command should return simply a boolean value after checking if the vectors provided are linearly independent or not
- */
-
-//TODO : Remove later
-// One potential appraoch --> allow for various options, we can check if there exists a solution given the vectors, meaning, we will need to form
 const { SlashCommandBuilder } = require("discord.js");
 const math = require("mathjs");
+// ! calculate whether a square matrix is linearly independent. If the determinant of a matrix is not equal to 0, that means it is linearly independent
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("linear-independence")
     .setDescription(
-      "determines if the vectors given are linearly independent to one another or not"
+      "determines if the vectors forming a matrix are linearly independent to one another or not"
     )
-    .addStringOption((option) => {
-      // TODO : Implement the logic here
-      throw new Error("Not yet implemented");
-    }),
+    // ! Note : No need for
+    .addStringOption((option) =>
+      // ? Note : The description cannot be too long or discord will throw in an error
+      option
+        .setName("matrix")
+        .setDescription(
+          "provide the square matrix of which you wish to check for indpendence"
+        )
+        .setRequired(true)
+    ),
+
+  execute: async function (interaction) {
+    try {
+      await interaction.deferReply({
+        ephemeral: true,
+      });
+
+      // all we simply check is the determinant of the given matrix and if it's not 0, then the vectors will be independent
+      const det = math.det(
+        math.matrix(JSON.parse(await interaction.options.get("matrix").value))
+      );
+      const determineIndependence = async () => {
+        if (det === 0) {
+          return false;
+        }
+        return true;
+      };
+
+      await interaction.editReply({
+        content: `Linear Independence : ${await determineIndependence()}`,
+      });
+
+      // retrieve and pass in the matrix that is recieved
+    } catch (error) {
+      console.log(`Error : ${error}`);
+      await interaction.editReply({
+        content: `There was an error executing this command.`,
+      });
+    }
+  },
 };
