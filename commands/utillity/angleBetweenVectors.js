99f367e2fc209c2addf4952024eb7f25b5a8564b
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-08-20 18:58:38 -0400

    feat:done with calculating the angle between two vectors

---
+++ commands/utillity/angleBetweenVectors.js
@@ -0,0 +1,59 @@
+// TODO : command for calculating the angle between vectors
+const { SlashCommandBuilder } = require("discord.js");
+const math = require("mathjs");
+
+// ! Implement the logic below
+// ? Reference formula :  θ = cos-1 [ (a. b) / (|a| |b|) ]
+// ? Reference https://mathjs.org/docs/reference/functions/acos.html --> mathjs formula for arccos
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("angle-between-vectors")
+    .setDescription("calculates the angle between two vectors")
+    // option 1 --> vector 1
+    .addStringOption((option) =>
+      option
+        .setName("vector1")
+        .setDescription("please provide the first vector in array format")
+        .setRequired(true)
+    )
+    // options 2 --> vector 2
+    .addStringOption((option) =>
+      option
+        .setName("vector2")
+        .setDescription("please provide the second vector in array format")
+        .setRequired(true)
+    ),
+  execute: async function (interaction) {
+    await interaction.deferReply({
+      ephemeral: true,
+    });
+    const vector1 = math.matrix(
+      JSON.parse(await interaction.options.get("vector1").value)
+    );
+    const vector2 = math.matrix(
+      JSON.parse(await interaction.options.get("vector2").value)
+    );
+    const resultInRadians = math.acos(
+      math.divide(
+        math.dot(vector1, vector2),
+        math.multiply(
+          math.sqrt(math.dot(vector1, vector1)),
+          math.sqrt(math.dot(vector2, vector2))
+        )
+      )
+    );
+
+    console.log(`Resulting value (radians) : ${resultInRadians}`);
+
+    // convert to degrees (radians * 180 / (pie)
+    const resultInDegrees = math.multiply(
+      resultInRadians,
+      math.divide(180, math.pi)
+    );
+    console.log(`Resulting values in (in degrees) : ${resultInDegrees}`);
+
+    await interaction.editReply({
+      content: `Successful exeuction of command!\n Angle (in radians) : ${resultInRadians} \n\n Angle (in degrees) : ${resultInDegrees}`,
+    });
+  },
+};
